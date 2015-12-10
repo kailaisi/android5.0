9f82ddc07fa197874e49cb2ac8043f064409b38e
wu <541018378@qq.com>
2015-12-10 17:03:28 +0800

    修改

---
+++ app/src/main/java/com/jiuan/android50/databinding/Person.java
@@ -0,0 +1,10 @@
+package com.jiuan.android50.databinding;
+
+/**
+ * Created by wu on 2015/12/10.
+ */
+public class Person {
+    public String name;
+    public String phone;
+    public boolean isVisible;
+}
