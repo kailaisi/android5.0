0f465cfd64cb610200773867f0f356e66f3ae742
wu <541018378@qq.com>
2015-12-10 17:03:11 +0800

    修改

--- app/src/main/java/com/jiuan/android50/MainActivity.java
+++ app/src/main/java/com/jiuan/android50/MainActivity.java
@@ -1,24 +1,27 @@
 
 package com.jiuan.android50;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.jiuan.android50.databinding.DataBindActivity;
 import com.jiuan.android50.design.MaterialActivity;
 import com.jiuan.android50.utils.StatusBarCompat;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private android.widget.Button btnmaterial;
     private android.widget.Button btnrecycleView;
+    private Button btndatabind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btndatabind = (Button) findViewById(R.id.btn_databind);
         this.btnrecycleView = (Button) findViewById(R.id.btn_recycleView);
         this.btnmaterial = (Button) findViewById(R.id.btn_material);
         initData();
@@ -36,6 +39,12 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,RecycleViewActivity.class));
+            }
+        });
+        btndatabind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DataBindActivity.class));
             }
         });
     }
